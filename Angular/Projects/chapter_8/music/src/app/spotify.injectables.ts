323edb4d6a7b9505421331c1e44761c8c9d42c84
Javid Shamloo <javid@bellsouth.net>
2017-11-20 13:42:08 -0500

    Remove duplicated directory

--- Angular/Projects/chapter_8/music/src/app/spotify.injectables.ts
+++
@@ -1,7 +0,0 @@
-import { SpotifyService, SPOTIFY_ID, SPOTIFY_SECRET } from './spotify.service';
-
-export const spotifyInjectables: Array<any> = [
-	{provide: SpotifyService, useClass: SpotifyService},
-	{provide: SPOTIFY_ID, useValue: SPOTIFY_ID},
-	{provide: SPOTIFY_SECRET, useValue: SPOTIFY_SECRET}
-];
