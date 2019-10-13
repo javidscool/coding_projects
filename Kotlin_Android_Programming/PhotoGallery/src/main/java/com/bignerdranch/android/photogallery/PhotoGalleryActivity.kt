9b6e8f01f7dc3cc9efb285ba114f0031d517ec5c
Your Name <you@example.com>
2019-10-13 15:51:32 -0400

    adding kotlin

---
+++ Kotlin_Android_Programming/PhotoGallery/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.kt
@@ -0,0 +1,29 @@
+package com.bignerdranch.android.photogallery
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PhotoGalleryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_gallery)
+
+        // checking whether fragment is already hosted and creating it if not
+        val isFragmentContainerEmpty = savedInstanceState == null
+        if (isFragmentContainerEmpty) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragmentContainer, PhotoGalleryFragment.newInstance())
+                .commit()
+        }
+    }
+
+    companion object {
+        fun newIntent(context: Context): Intent {
+            return Intent(context, PhotoGalleryActivity::class.java)
+        }
+    }
+}
